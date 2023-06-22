df8d3b9fe0a83b87b8074d9fdbee2ae06d46e239
gruntcole <gruntcole1998@gmail.com>
2023-06-22 19:47:38 -0400

    finsished first version

--- app.js
+++ app.js
@@ -1,10 +1,81 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
+const postBank = require("./postBank");
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(express.static('public'));
 
-const PORT = 1337;
+app.get("/", (req, res) => {
+  const posts = postBank.list();
+
+  const html =`<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map(post => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            <a href="/posts/${post.id}">${post.title}</a>
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+      ).join('')}
+    </div>
+  </body>
+</html>`;
+
+  res.send(html);
+});
+
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+  if (!post.id) {
+    throw new Error('Not Found')
+  }
+  res.send(`<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            ${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+          <div>${post.content}</div>
+        </div>
+    </div>
+  </body>
+</html>`
+)});
+
+app.use((err, req, res, next) => {
+  console.error(err)
+  res.status(err.status || 500).send(`${err}`)
+})
+
+const { PORT = 1337 } = process.env;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
+
+app.use(morgan("dev"));
